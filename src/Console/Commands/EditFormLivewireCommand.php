056df9f9d30e67620404a6ec4e0e49b6d26dda06
Santos Sabanari <santos.sabanari@yahoo.com>
2020-11-27 04:29:52 +0700

    wip

--- src/Console/Commands/EditFormLivewireCommand.php
+++ src/Console/Commands/EditFormLivewireCommand.php
@@ -42,8 +42,8 @@ class EditFormLivewireCommand extends GeneratorCommand
         $camel = Str::camel($this->argument('name'));
         $stub = str_replace('{{camelCase}}', $camel, $stub);
 
-        $camel = Str::lower($this->argument('name'));
-        $stub = str_replace('{{lowerCase}}', $camel, $stub);
+        $lower = Str::lower($this->argument('name'));
+        $stub = str_replace('{{lowerCase}}', $lower, $stub);
 
         $firstword = Str::title(str_replace('-',' ',$this->argument('name')));
         $stub = str_replace('{{FirstWordCase}}', $firstword, $stub);
