e036a8f77321c47570903b726ecc409d6604afa8
Santos Sabanari <santos.sabanari@yahoo.com>
2020-12-07 05:32:31 +0700

    wip

--- resources/views/components/utils/modal-dialog.blade.php
+++ resources/views/components/utils/modal-dialog.blade.php
@@ -1,12 +1,13 @@
-@props(['title' => 'Modal title', 'type' => 'primary', 'id' => md5(\Carbon\Carbon::now() . Str::random(40))])
+@props(['title' => 'Modal title', 'type' => 'primary', 'size' => '', 'id' => md5(\Carbon\Carbon::now() . Str::random(40)), 'scripts' => false])
 
 <button class="btn btn-{{$type}}" type="button" data-toggle="modal" data-target="#{{$id}}">Tambah</button>
-<div class="modal fade" id="{{$id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-{{$type}}" role="document">
+
+<div class="modal fade" id="{{$id}}" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-{{$type}} {{$size==''?'':"modal-$size"}}" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title">{{$title}}</h4>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                <button class="close text-white" type="button" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
             </div>
@@ -16,3 +17,15 @@
         </div>
     </div>
 </div>
+
+@push('after-scripts')
+    <script>
+        window.addEventListener('closeModal', event => {
+            $('#tambah').trigger('click');
+        })
+    </script>
+
+    @if($scripts)
+        {{ $scripts }}
+    @endif
+@endpush
