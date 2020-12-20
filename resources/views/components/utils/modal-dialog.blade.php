1dd9669771b3eca1445f05a659e23c2cbce31454
Santos Sabanari <santos.sabanari@yahoo.com>
2020-12-21 04:40:25 +0700

    tambah attribute di modal dialog

--- resources/views/components/utils/modal-dialog.blade.php
+++ resources/views/components/utils/modal-dialog.blade.php
@@ -1,6 +1,7 @@
 @props(['title' => 'Modal title', 'label' => 'Tambah', 'type' => 'primary', 'size' => '', 'id' => md5(\Carbon\Carbon::now() . Str::random(40)), 'scripts' => false])
 
-<button class="btn btn-{{$type}}" type="button" data-toggle="modal" data-target="#{{$id}}">{{ $label }}</button>
+<button class="btn btn-{{$type}}" type="button" data-toggle="modal"
+        data-target="#{{$id}}" {{ $attributes }}>{{ $label }}</button>
 
 <div class="modal fade" id="{{$id}}" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-{{$type}} {{$size==''?'':"modal-$size"}}" role="document">
