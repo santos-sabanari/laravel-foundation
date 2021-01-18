08acdf6e38b0246d63b2857f28faf9b268a11298
Santos Sabanari <santos.sabanari@yahoo.com>
2021-01-19 04:06:38 +0700

    tambah modal blade

---
+++ resources/views/components/utils/modal.blade.php
@@ -0,0 +1,41 @@
+@props(['title' => 'Modal title', 'type' => 'primary', 'size' => '', 'id' => '', 'scripts' => false])
+
+<div class="modal fade" id="{{$id}}" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-{{$type}} {{$size==''?'':"modal-$size"}}" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">
+                    {{$title}}
+                </h4>
+                <button class="close text-white" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+</div>
+
+@push('after-scripts')
+    <script>
+        window.addEventListener('close{{$id}}', event => {
+            $('#{{$id}}').modal('hide');
+        })
+
+        window.addEventListener('show{{$id}}', event => {
+            $('#{{$id}}').modal('show');
+        })
+
+        $(document).ready(function(){
+            $("#{{$id}}").on('hidden.coreui.modal', function(){
+                livewire.emit('forcedClose{{$id}}');
+            });
+        });
+    </script>
+
+    @if($scripts)
+        {{ $scripts }}
+    @endif
+@endpush
