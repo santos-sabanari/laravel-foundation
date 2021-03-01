d96f1ead69df665b3a78a50f6c8ab5baf4a1f173
Santos Sabanari <santos.sabanari@yahoo.com>
2021-03-01 16:44:47 +0700

    wip

--- resources/views/components/utils/modal-bootstrap-custom.blade.php
+++ resources/views/components/utils/modal-bootstrap-custom.blade.php
@@ -1,11 +1,7 @@
 @props(['size' => '', 'id' => '', 'scripts' => false])
 
 <div class="modal fade" id="{{$id}}" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog {{$size==''?'':"modal-$size"}}" role="document">
-        <div class="modal-content">
-            {{ $slot }}
-        </div>
-    </div>
+    {{ $slot }}
 </div>
 
 @push('after-scripts')
